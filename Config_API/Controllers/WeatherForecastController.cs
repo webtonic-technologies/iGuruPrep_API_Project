1b10d55ab5d03618042b3fc3c04b51371764ad92
VinitaWebtonic <vinita@webtonictechnologies.com>
2024-03-14 16:44:09 +0530

    changes1

--- Config_API/Controllers/WeatherForecastController.cs
+++ Config_API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Config_API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "1","2","3","4"
+            "1","2","3"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
